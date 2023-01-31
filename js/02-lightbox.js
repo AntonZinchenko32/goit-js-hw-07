ef3e5efc5a7e02d81096a7bab0cd643e2aa1a062
AntonZinchenko32 <zinchenko309@gmail.com>
2023-01-31 18:57:20 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const myGallery = document.querySelector(".gallery");
+
+createGallery();
+
+// Створення,рендер розмітки, додавання єлементів до гаоереї
+
+function createGallery() {
+    const items = [];
+
+    galleryItems.forEach(item => {
+        const { preview, original, description } = item;
+
+        const link = document.createElement("a");
+        link.classList.add("gallery__item");
+        link.setAttribute("href", original);
+
+        const image = document.createElement('img');
+        image.classList.add("gallery__image");
+
+        image.src = preview;
+        image.alt = description;
+
+        link.append(image);
+
+        items.push(link);
+    });
+
+    myGallery.append(...items);
+}
+
+
 console.log(galleryItems);
